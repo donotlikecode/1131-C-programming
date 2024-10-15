71e9cf89a4f969ae1c65a8a3a70d89954d2aea49
donotlikecode <52683200+donotlikecode@users.noreply.github.com>
2024-10-15 16:51:09 +0800

    Add files via upload

---
+++ EX5-1.cpp
@@ -0,0 +1,31 @@
+﻿// EX5-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    float HP = 50;
+
+    while (HP < 100)
+    {
+        HP++;
+        printf("HP=$.1f\n", HP);
+    }
+
+    for (int HP = 0; HP < 100; HP++)
+    {
+        printf("HP=%i\n", HP);
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
