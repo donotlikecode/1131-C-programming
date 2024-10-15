63534b5923561a51990affd3c6ec7a6504c12e40
donotlikecode <52683200+donotlikecode@users.noreply.github.com>
2024-10-16 07:43:12 +0800

    Create 輸出因數(還差1和自己).c

---
+++ examples/輸出因數(還差1和自己).c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main(void) 
+{
+    int num, div;
+    int isprime;
+    printf("Enter an posittve integer\n");
+    //scanf("%d", &num);
+    
+    while (scanf("%d", &num) == 1) //用來看有沒有讀到東西
+    
+    {
+        for (div = 2, isprime = 1; div * div <= num; div++)
+        {
+            if (num % div == 0)
+            {
+                printf("%d,  ", div);
+                isprime = 0;
+            }
+        }
+        for (; div >= 2; div--)
+        {
+            if (num % div == 0)
+            {
+                printf("%d, ", num / div);
+            }
+        }
+        if (isprime)
+            printf("%d is a prime number!\n", num);
+        else
+            printf("\b\b are divisors of %d\n", num);
+    }
+
+}
