c0e675361ff9f69a315ae7827fc60a19121ca94b
donotlikecode <52683200+donotlikecode@users.noreply.github.com>
2024-10-16 00:42:20 +0800

    Update 從1開始加到使用者輸入的數字.c

--- examples/從1開始加到使用者輸入的數字.c
+++ examples/從1開始加到使用者輸入的數字.c
@@ -9,15 +9,20 @@ int main(void)
     printf("Enter a number from 1 to 100\n");
     scanf("%d", &uppernum);
     int sum = 0;
+    int sumsqu = 0;
+    int sumcubi = 0;
         
         
-        
-    for (int num = 1; num <= uppernum; num++)
+    for (int num = 1; num <= uppernum; num = num +1)
     {
         sum = sum + num;
-        
+        sumsqu = sumsqu + num * num;
+        sumcubi = sumcubi + num * num * num;
     }
     
-    printf("the sum from 1 to %d is %d", uppernum, sum);
+    printf("the sum from 1 to %d is %d\n", uppernum, sum);
+    printf("the sum from 1 to %d is %d\n", uppernum, sumsqu);
+    printf("the sum from 1 to %d is %d\n", uppernum, sumcubi);
+
 
  }
