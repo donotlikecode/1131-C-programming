cc70ed0585293f4048789184192af81a708630c9
donotlikecode <52683200+donotlikecode@users.noreply.github.com>
2024-10-15 18:00:45 +0800

    Update EX4-3.c

--- L4  program control 1/EX4-3.c
+++ L4  program control 1/EX4-3.c
@@ -8,7 +8,7 @@
 
 int main()
 {
-    float Ox[5] = { 2,4,6,8,10 };
+    float Ox[5] = { 2,4,6,8,10 };  //障礙物在(2,1) (4,3) (6,5) (8,7) (10,9)
     float Oy[5] = { 1,3,5,7,9 };
     float vx = 10;
     float vy = 5;
@@ -20,25 +20,26 @@ int main()
     float v = 10;
     float w = 0.174;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)  
     {
-        printf("obstacle position is (%.2f,%.2f)\n", Ox[i], Oy[i]);
+        printf("obstacle position is (%.2f,%.2f)\n", Ox[i], Oy[i]);  //印出障礙物的位置
     }
-    printf("(VX,VY,dt)=(%f,%f,%f)\n", vx, vy, dt);
+    printf("(VX,VY,dt)=(%f,%f,%f)\n", vx, vy, dt);  //印出現在的狀態
 
-    for (int i = 0; i < 5; i++)
+
+    for (int i = 0; i < 5; i++)  //i=0 Ox[0]
     {
-        Ox[i] = Ox[i] + vx * dt;
+        Ox[i] = Ox[i] + vx * dt;                 //斷點設在這裏去看
         Oy[i] = Oy[i] + vy * dt;
         printf("next obstacle position is (%.2f,%.2f)\n", Ox[i], Oy[i]);
     }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)  //算機器人的位置
     {
         x = x + v * cos(theta) * dt;
         y = y + v * sin(theta) * dt;
         theta = theta + w * dt;
-        printf("Robot position is (%.2f,%.2f,%.2f)\n, x, y, theta");
+        printf("Robot position is (%.2f,%.2f,%.2f)\nx, y, theta");
     }
 
 }
