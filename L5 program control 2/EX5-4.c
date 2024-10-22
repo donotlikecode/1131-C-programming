0d16aae151d2d5b456d05314125b314010a9481c
donotlikecode <52683200+donotlikecode@users.noreply.github.com>
2024-10-22 20:57:24 +0800

    Create EX5-4.c

---
+++ L5 program control 2/EX5-4.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int key = 5;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("i=%d\n", i);
+        if (i == key)
+        {
+            printf("got key=%d\n", key);
+            break;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("i=%d\n", i);
+        if (i == key)
+        {
+            printf("got key=%d\n", key);
+            continue;
+        }
+    }
+}
